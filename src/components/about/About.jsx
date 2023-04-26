4877508691d575f55d73d030905ef8aa79b7013b
liridonig <doo.nii@hotmail.com>
2023-04-26 17:04:10 +0200

    About me text edit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./About.css";
-import { FaUser } from "react-icons/fa";
+import { FaUser, FaArrowDown } from "react-icons/fa";
+import { Link } from "react-scroll";
+import RoundImg from "../../assets/round-text.png";
 
 const About = () => {
   return (
@@ -11,17 +13,38 @@ const About = () => {
             <FaUser></FaUser> ABOUT
           </h4>
           <h3 className="btn-info-subtitle standby">
-            Every great design begin with an even
-            <span> better story</span>
+            Welcome to my
+            <span> portfolio!</span>
           </h3>
           <p className="standby">
-            Since beginning my journey as a freelance designer nearly 8 years
-            ago, I've done remote work for agencies, consulted for startups, and
-            collaborated with talented people to create digital products for
-            both business and consumer use. I'm quietly confident, naturally
-            curious, and perpetually working on improving my chopsone design
-            problem at a time.
+            With over 3+ years of experience, I have developed a keen eye for
+            creating modern, user-friendly websites that meet the needs of
+            businesses and individuals alike. My approach to web development is
+            centered on understanding the unique goals and vision of each
+            project, and crafting customized solutions that exceed expectations.
+            Whether it's building a brand-new website from scratch or revamping
+            an existing one, I take pride in delivering high-quality work that
+            meets the highest standards of excellence.
           </p>
+          <p className="standby">
+            Take a look at some of the projects in my portfolio to see examples
+            of my work, and feel free to reach out if you have any questions or
+            would like to discuss a potential collaboration. I look forward to
+            working with you!
+          </p>
+          <div className="projects-intro standby">
+            <Link
+              to="projects"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+            >
+              <img src={RoundImg}></img>
+              <FaArrowDown></FaArrowDown>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
